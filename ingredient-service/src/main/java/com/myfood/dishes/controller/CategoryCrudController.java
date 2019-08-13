b6d3b1e5c8aee9751b3cc4b539ca9a094342402e
rdv <rakovdim@gmail.com>
2019-08-14 03:09:14 +0400

    INGREDIENT MODEL ADDED

--- ingredient-service/src/main/java/com/myfood/dishes/controller/CategoryCrudController.java
+++ ingredient-service/src/main/java/com/myfood/dishes/controller/CategoryCrudController.java
@@ -5,8 +5,8 @@ import com.myfood.dishes.controller.dto.CategoryMapper;
 import com.myfood.dishes.service.search.CategorySearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,8 +26,8 @@ public class CategoryCrudController {
         this.categoryMapper = categoryMapper;
     }
 
-    @GetMapping("/top")
-    public List<CategoryDTO> getTop(@RequestParam int count) {
+    @GetMapping("/top/{count}")
+    public List<CategoryDTO> getTop(@PathVariable int count) {
         return categorySearchService.findTopCategories(count).stream().map(categoryMapper::categoryDTOfromDO).collect(Collectors.toList());
     }
 }
